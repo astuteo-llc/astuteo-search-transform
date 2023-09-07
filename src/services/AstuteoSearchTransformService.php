6a8dc7e8ff4ec3f92bb541f7ea5b243a8c41afd5
dustinwalker <me@dustinwalker.com>
2023-09-07 12:30:43 -0500

    ADD: updates from C4 + better multi-site support

--- src/services/AstuteoSearchTransformService.php
+++ src/services/AstuteoSearchTransformService.php
@@ -11,17 +11,35 @@ namespace astuteo\astuteosearchtransform\services;
 
 use Craft;
 use craft\base\Component;
+use craft\helpers\StringHelper;
+
 
 /**
+ * AstuteoSearchTransformService Service
+ *
+ * All of your plugin’s business logic should go in services, including saving data,
+ * retrieving data, etc. They provide APIs that your controllers, template variables,
+ * and other plugins can interact with.
+ *
+ * https://craftcms.com/docs/plugins/services
+ *
  * @author    Astuteo
  * @package   AstuteoSearchTransform
  * @since     1.0.0
  */
 class AstuteoSearchTransformService extends Component
 {
-    // Constant
+
+    // Constants
     // Fields to extract text from
-    const TEXT_FIELDS = ['text', 'plaintext'];
+    const TEXT_FIELDS = [
+        'text',
+        'plaintext'
+    ];
+
+    // Public Methods
+    // =========================================================================
+
     public function extractMatrixText($matrix, $handle, $include = [])
     {
         return $this->cleanText($this->matrixCopy($matrix, $handle, $include));
@@ -29,12 +47,11 @@ class AstuteoSearchTransformService extends Component
 
     public function extractEntryText($entry, $include = [])
     {
-        $fields = Craft::$app->getEntries()->getEntryById($entry->id)
-            ->fieldValues;
+        $fields = Craft::$app->getEntries()->getEntryById($entry->id)->fieldValues;
         $text = '';
-        foreach ($fields as $field => $value) {
+        foreach ( $fields as $field => $value) {
             switch ($field) {
-                case in_array($field, $include):
+                case in_array($field, $include);
                     $text = ' ' . $text . ' ' . $this->parseField($value);
             }
         }
@@ -42,83 +59,106 @@ class AstuteoSearchTransformService extends Component
         return $this->cleanText($text);
     }
 
-    private function parseField($fieldVal)
-    {
+    private function parseField($fieldVal) {
         return $fieldVal;
     }
 
-    public function matrixCopy($entry, $handle, $include)
+    public function matrixCopy($entry, $handle, $include): string
     {
         $text = '';
         $textBlocks = [];
+        $siteId = $entry->siteId;
+
         if ($entry->$handle) {
             foreach ($entry->$handle->all() as $block) {
                 $blockHandles = $block->type->handle;
-                switch ($blockHandles) {
-                    case in_array($blockHandles, $include):
-                        $fields = Craft::$app
-                            ->getMatrix()
-                            ->getBlockById($block->id)->fieldValues;
-                        array_push($textBlocks, $this->parseFields($fields));
-                        break;
+                if (in_array($blockHandles, $include)) {
+                    $matrixBlock = Craft::$app->getMatrix()->getBlockById($block->id, $siteId);
+                    if ($matrixBlock !== null) {
+                        $fields = $matrixBlock->fieldValues;
+                        $textBlocks[] = $this->parseFields($fields);
+                    } else {
+                        Craft::error("Matrix block with ID {$block->id} not found for site ID {$siteId}.", __METHOD__);
+                    }
                 }
             }
         }
+
         foreach ($textBlocks as $textBlock) {
             if ($textBlock) {
-                $text = $text . ' ' . $textBlock;
+                $text .= ' ' . $textBlock;
             }
         }
         return $this->cleanText($text);
     }
 
+
+
+
     // Extract Direct Entry Text Values
-    private function parseEntryFields($fields, array $include)
-    {
-        return $this->cleanText($this->parseFields($fields));
+    private function parseEntryFields($fields, array $include) {
+        return $this->cleanText( $this->parseFields($fields));
     }
 
-    public function parseFields($fields, $related = true)
+    // Returns values from text fields. Accessed by Entries
+    // Categories, Matrix blocks
+    public function parseFields(array $fields, bool $related = true): string
     {
         $text = '';
         foreach ($fields as $field) {
+            // Replace match with if/elseif for determining $check
             if (isset($field->elementType)) {
                 $check = $field->elementType;
             } elseif (is_object($field)) {
                 $check = get_class($field);
+            } elseif (is_array($field)) {
+                $check = 'array';
             } else {
                 $check = 'string';
             }
+
+            // Replace the second match with switch
             switch ($check) {
                 case 'craft\elements\Entry':
-                    if ($related) {
-                        $text = $this->parseRelatedEntries($field);
-                    }
+                    $text .= $related ? ' ' . $this->parseRelatedEntries($field) : '';
                     break;
                 case 'craft\redactor\FieldData':
-                    $text = $text . ' ' . $field;
-                    break;
                 case 'string':
-                    $text = $text . ' ' . $field;
+                    $text .= ' ' . $field;
+                    break;
+                case 'array':
+                    $text .= ' ' . $this->flattenArray($field);
+                    break;
                 default:
+                    break;
             }
         }
         return $this->cleanText($text);
     }
 
-    private function cleanText(string $text)
+
+    private function cleanText(string $text): string
     {
-        return strip_tags(trim($text));
+        //@NOTE: I'm not sure if we want to encode this, but testing it
+        $text = html_entity_decode($text); // Decode HTML entities
+        $text = str_replace('&nbsp;', ' ', $text); // Convert non-breaking spaces to regular spaces
+        $text = str_replace('<', ' <', $text); // Add space before '<' to ensure spaces between tags
+
+        $text = strip_tags($text); // Remove HTML tags
+
+        $text = StringHelper::collapseWhitespace($text); // Collapse whitespace
+
+        return $text;
     }
 
-    private function parseRelatedEntries($relatedEntries)
-    {
+    // Pull out related entry text fields
+    private function parseRelatedEntries($relatedEntries) {
         $text = '';
         foreach ($relatedEntries->all() as $item) {
-            $fields = Craft::$app->getEntries()->getEntryById($item->id)
-                ->fieldValues;
+            $fields = Craft::$app->getEntries()->getEntryById($item->id)->fieldValues;
             $text = $this->parseFields($fields, false);
         }
         return $this->cleanText($text);
     }
+
 }
